3aa66ea57dd3d239642340d047cbe87a80d7895e
Peter Cottle <petermcottle@gmail.com>
2012-10-20 19:12:36 -0700

    escaping issue

--- src/commandViews.js
+++ src/commandViews.js
@@ -170,6 +170,7 @@ var CommandPromptView = Backbone.View.extend({
     _.each(value.split(';'), _.bind(function(command, index) {
       command = command.replace(/^(\s+)/, '');
       command = command.replace(/(\s+)$/, '');
+      command = command.replace(/&quot;/g, '"');
       command = _.escape(command);
 
       if (index > 0 && !command.length) {
