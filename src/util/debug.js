b21185dd342bdbbea5eacd5d56ec7d6e9bb03b64
Peter Cottle <petermcottle@gmail.com>
2012-12-14 01:33:10 -0800

    everything moved somewhere now

---
+++ src/util/debug.js
@@ -0,0 +1,19 @@
+var toGlobalize = {
+  Tree: require('../tree'),
+  Visuals: require('../visuals'),
+  Git: require('../git'),
+  CommandModel: require('../models/commandModel'),
+  Levels: require('../levels'),
+  Constants: require('../constants'),
+  Collections: require('../collections'),
+  Async: require('../animation'),
+  AnimationFactory: require('../animation/animationFactory'),
+  Main: require('../app/main')
+};
+
+_.each(toGlobalize, function(module) {
+  _.extend(window, module);
+});
+
+window.events = toGlobalize.Main.getEvents();
+
