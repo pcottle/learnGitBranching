6bf593b787363e2d3856ad2ffa9eae883b214d46
Shao-Chung Chen <dannvix@gmail.com>
2013-02-17 12:51:57 +0800

    fix a typo in rebase1

--- src/levels/rebase/1.js
+++ src/levels/rebase/1.js
@@ -20,7 +20,7 @@ exports.level = {
             "",
             "Upper management is making this a bit trickier though -- they want the commits to all be in sequential order. So this means that our final tree should have `C7'` at the bottom, `C6'` above that, etc etc, etc all in order.",
             "",
-            "If you mess up along the way, feel free to use `reset` to start over again. Be sure to check our our solution and see if you can do it in fewer commands!"
+            "If you mess up along the way, feel free to use `reset` to start over again. Be sure to check out our solution and see if you can do it in fewer commands!"
           ]
         }
       }
