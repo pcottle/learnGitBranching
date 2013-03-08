b3c6b6de66764ef9151440a082c946e896edefd6
Peter Cottle <petermcottle@gmail.com>
2013-03-07 20:11:52 -0800

    dialog

---
+++ src/js/dialogs/confirmShowSolution.js
@@ -0,0 +1,13 @@
+exports.dialog = {
+  'en_US': [{
+    type: 'ModalAlert',
+    options: {
+      markdowns: [
+        '## Are you sure you want to see the solution?',
+        '',
+        'I believe in you! You can do it'
+      ]
+    }
+  }]
+};
+
