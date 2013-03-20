36ad66e11eaf059c705c15a4339b8dd68eb63c78
Aaron Schrab <aaron@schrab.com>
2013-03-20 17:03:43 -0400

    Replace incorrect information about HEAD
    Avoid stating that changing HEAD alters the working tree.  Although that
    is a common next step they are not actually the same action and can be
    done independently (e.g. `git reset` without `--hard`).
    
    Also state that committing while on a branch only alters the branch,
    without actually changing HEAD; HEAD only gets the new commit because it
    points to the branch rather than a specific commit.

--- src/levels/rampup/1.js
+++ src/levels/rampup/1.js
@@ -34,11 +34,11 @@ exports.level = {
             "markdowns": [
               "## HEAD",
               "",
-              "First we have to talk about \"HEAD.\" HEAD is the symbolic name for the currently checked out commit -- it's essentially what commit you're working on top of.",
+              "First we have to talk about \"HEAD\". HEAD is the symbolic name for the currently checked out commit -- it's essentially what commit you're working on top of.",
               "",
-              "The working directory will always match the current state of HEAD, so by moving HEAD, you actually change the contents of your directory.",
+              "HEAD always points to the most recent commit which is reflected in the working tree. Most git commands which make changes to the working tree will start by changing HEAD.",
               "",
-              "Normally HEAD points to a branch name (like `bugFix`). When you commit, both bugFix and HEAD move together"
+              "Normally HEAD points to a branch name (like bugFix). When you commit, the status of bugFix is altered and this change is visible through HEAD."
             ]
           }
         },
