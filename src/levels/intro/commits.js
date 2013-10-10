c2cab4c12ab2a5913278c0a7eb343ae0c8e96d7a
Peter Cottle <pcottle@fb.com>
2013-10-09 21:18:25 -0700

    Resolves #133 content for first lesson slide

--- src/levels/intro/commits.js
+++ src/levels/intro/commits.js
@@ -29,7 +29,9 @@ exports.level = {
               "## Git Commits",
               "A commit in a git repository records a snapshot of all the files in your directory. It's like a giant copy and paste, but even better!",
               "",
-              "Git wants to keep commits as lightweight as possible though, so it doesn't just blindly copy the entire directory every time you commit. It can (when possible) compress a commit as a set of changes, or a \"delta\", from one version of the repository to the next. That's why most commits have a parent commit above them -- you'll see this later in our visualizations.",
+              "Git wants to keep commits as lightweight as possible though, so it doesn't just blindly copy the entire directory every time you commit. It can (when possible) compress a commit as a set of changes, or a \"delta\", from one version of the repository to the next.",
+              "",
+              "Git also maintains a history of which commits were made when. That's why most commits have ancestor commits above them -- we designate this with arrows in our visualization. Maintaining history is great for everyone working on the project!",
               "",
               "It's a lot to take in, but for now you can think of commits as snapshots of the project. Commits are very lightweight and switching between them is wicked fast!"
             ]
