df0256d26b9d1f18c5a156bf70474b17b27d7939
Peter Cottle <pcottle@fb.com>
2013-10-11 12:10:56 -0700

    More clowntown on Issue #136 fix jasmine test

--- spec/git.spec.js
+++ spec/git.spec.js
@@ -149,10 +149,10 @@ describe('Git', function() {
     );
   });
 
-  it('does not fast forward on merge if specified', function() {
+  it('if no-ff is specified, will always make a merge commit', function() {
     expectTreeAsync(
       'git commit; go -b side HEAD~1; git commit; git merge master; go master; git merge side --no-ff',
-      '{"branches":{"master":{"target":"C4","id":"master","remoteTrackingBranchID":null},"feature":{"target":"C3","id":"feature","remoteTrackingBranchID":null}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"},"C2":{"parents":["C1"],"id":"C2"},"C3":{"parents":["C2"],"id":"C3"},"C4":{"parents":["C1","C3"],"id":"C4"}},"HEAD":{"target":"master","id":"HEAD"}}'
+      '{"branches":{"master":{"target":"C5","id":"master","remoteTrackingBranchID":null},"side":{"target":"C4","id":"side","remoteTrackingBranchID":null}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"},"C2":{"parents":["C1"],"id":"C2"},"C3":{"parents":["C1"],"id":"C3"},"C4":{"parents":["C2","C3"],"id":"C4"},"C5":{"parents":["C2","C4"],"id":"C5"}},"HEAD":{"target":"master","id":"HEAD"}}'
     );
   });
 });
