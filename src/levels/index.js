27ea21d39ac3bff769dc3059f371222167de4c2e
Peter Cottle <pcottle@fb.com>
2013-10-23 18:46:40 -0700

    remove place since not correct will update

--- src/levels/index.js
+++ src/levels/index.js
@@ -39,8 +39,7 @@ exports.levelSequences = {
   remoteAdvanced: [
     require('./remote/pushManyFeatures').level,
     require('./remote/mergeManyFeatures').level,
-    require('./remote/tracking').level,
-    require('./remote/place').level
+    require('./remote/tracking').level
   ]
 };
 
