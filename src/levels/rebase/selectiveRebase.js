98ae0ff7bd7ccb7176095a181a59b8a2bd5b3417
Carl X. Su <bcbcarl@gmail.com>
2013-12-07 11:30:49 +0800

    levels/rebase 去簡體化

--- src/levels/rebase/selectiveRebase.js
+++ src/levels/rebase/selectiveRebase.js
@@ -11,14 +11,14 @@ exports.level = {
     "en_US": "Branch Spaghetti",
     "ja": "ブランチスパゲッティ",
     "zh_CN": "分支浆糊",
-    "zh_TW": "分支浆糊"
+    "zh_TW": "分支漿糊"
   },
   "hint": {
     "en_US": "Make sure to do everything in the proper order! Branch one first, then two, then three",
     "ja": "全て正しい順番で処理すること！oneが最初で、次がtwo、最後にthreeを片付ける。",
     "ko": "이 문제를 해결하는 방법은 여러가지가 있습니다! 체리픽(cherry-pick)이 가장 쉽지만 오래걸리는 방법이고, 리베이스(rebase -i)가 빠른 방법입니다",
     "zh_CN": "确保你是按照正确的顺序来操作！先操作分支 `one`, 然后 `two`, 最后才是 `three`",
-    "zh_TW": "确保你是按照正确的顺序来操作！先操作分支 `one`, 然后 `two`, 最后才是 `three`"
+    "zh_TW": "確保你是按照正確的順序來操作！先操作分支 `one`, 然後 `two`, 最後才是 `three`"
   },
   "startDialog": {
     "en_US": {
@@ -89,13 +89,13 @@ exports.level = {
             "markdowns": [
               "## Branch Spaghetti",
               "",
-              "哇塞大神！这关我们要来点不同的！",
+              "哇塞大神！這關我們要來點不同的！",
               "",
-              "现在我们的 `master` 分支是比 `one` `two` 和 `three` 要多几个提交。出于某种原因，我们需要把其他三个分支更新到 master 分支上新近的几个不同提交上。（update these three other brances with modified versions of the last few commits on master）",
+              "現在我們的 `master` 分支是比 `one` `two` 和 `three` 要多幾個提交。出於某種原因，我們需要把其他三個分支更新到 master 分支上新近的幾個不同提交上。（update these three other brances with modified versions of the last few commits on master）",
               "",
-              "分支 `one` 需要重新排序和撤销， `two` 需要完全重排，而 `three` 只需要提交一次。",
+              "分支 `one` 需要重新排序和撤銷， `two` 需要完全重排，而 `three` 只需要提交一次。",
               "",
-              "慢慢摸索会找到答案的 —— 你完事记得用 `show solution` 看看我们的答案哦。"
+              "慢慢摸索會找到答案的 —— 你完事記得用 `show solution` 看看我們的答案哦。"
             ]
           }
         }
