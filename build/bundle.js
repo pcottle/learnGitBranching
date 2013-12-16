5aac7371f75fee84066d51c8eacca05b7eba0e25
Peter Cottle <pcottle@fb.com>
2013-12-15 17:26:52 -0800

    rebuild

--- build/bundle.js
+++ build/bundle.js
@@ -11342,7 +11342,7 @@ var assertNotCheckedOut = function(engine, ref) {
 var assertIsBranch = function(engine, ref) {
   assertIsRef(engine, ref);
   var obj = engine.refs[ref];
-  if (obj.get('type') !== 'branch') {
+  if (!obj || obj.get('type') !== 'branch') {
     throw new GitError({
       msg: intl.todo(
         ref + ' is not a branch'
@@ -28121,7 +28121,7 @@ var assertNotCheckedOut = function(engine, ref) {
 var assertIsBranch = function(engine, ref) {
   assertIsRef(engine, ref);
   var obj = engine.refs[ref];
-  if (obj.get('type') !== 'branch') {
+  if (!obj || obj.get('type') !== 'branch') {
     throw new GitError({
       msg: intl.todo(
         ref + ' is not a branch'
