8fb679333b208d0c5fbb1c341f0628bdf8905a81
Peter Cottle <pcottle@fb.com>
2014-01-06 10:10:10 -0800

    rebuild for prod

--- build/bundle.js
+++ build/bundle.js
@@ -7655,7 +7655,7 @@ var assertNotCheckedOut = function(engine, ref) {
 
 var assertIsBranch = function(engine, ref) {
   assertIsRef(engine, ref);
-  var obj = engine.refs[ref];
+  var obj = engine.resolveID(ref);
   if (!obj || obj.get('type') !== 'branch') {
     throw new GitError({
       msg: intl.todo(
@@ -7667,7 +7667,7 @@ var assertIsBranch = function(engine, ref) {
 
 var assertIsRemoteBranch = function(engine, ref) {
   assertIsRef(engine, ref);
-  var obj = engine.refs[ref];
+  var obj = engine.resolveID(ref);
 
   if (obj.get('type') !== 'branch' ||
       !obj.getIsRemote()) {
@@ -7694,7 +7694,7 @@ var assertOriginSpecified = function(generalArgs) {
 
 var assertBranchIsRemoteTracking = function(engine, branchName) {
   branchName = crappyUnescape(branchName);
-  if (!engine.refs[branchName]) {
+  if (!engine.resolveID(branchName)) {
     throw new GitError({
       msg: intl.todo(branchName + ' is not a branch!')
     });
@@ -8304,7 +8304,7 @@ var commandConfig = {
           // can be created on demand but we at least need this to be a source
           // locally otherwise we will fail
           assertIsRef(engine, firstArg);
-          sourceObj = engine.refs[firstArg];
+          sourceObj = engine.resolveID(firstArg);
         } else {
           // since they have not specified a source or destination, then
           // we source from the branch we are on (or HEAD)
@@ -29314,4 +29314,4 @@ exports.level = {
   }
 };
 
-},{}]},{},[11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96])
+},{}]},{},[11,13,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,49,75,77,78,79,80,81,82,83,84,85,86,87,89,90,91,92,93,94,95,96,88,76])
