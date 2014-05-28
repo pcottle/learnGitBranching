0a2d787572d96664f85e92882f2578cf97fbbca5
Peter Cottle <pcottle@fb.com>
2014-05-27 20:49:23 -0700

    rebuild for prod

--- build/bundle.js
+++ build/bundle.js
@@ -6833,7 +6833,14 @@ function changeLocaleFromHeaders(langString) {
     var languages = langString.split(',');
     var desiredLocale;
     for (var i = 0; i < languages.length; i++) {
-      var lang = languages[i].slice(0, 2);
+      var header = languages[i].split(';')[0];
+      // first check the full string raw
+      if (intl.headerLocaleMap[header]) {
+        desiredLocale = intl.headerLocaleMap[header];
+        break;
+      }
+
+      var lang = header.slice(0, 2);
       if (intl.langLocaleMap[lang]) {
         desiredLocale = intl.langLocaleMap[lang];
         break;
@@ -12478,6 +12485,11 @@ var strings = require('../intl/strings').strings;
 
 var getDefaultLocale = exports.getDefaultLocale = function() {
   return 'en_US';
+};
+
+var headerLocaleMap = exports.headerLocaleMap = {
+  'zh-CN': 'zh_CN',
+  'zh-TW': 'zh_TW',
 };
 
 // resolve the messy mapping between browser language
@@ -35654,4 +35666,4 @@ exports.level = {
   }
 };
 
-},{}]},{},[11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,43,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96])
+},{}]},{},[11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96])
