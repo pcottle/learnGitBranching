339b61187491608f5a03586b1d1c485765ac23b7
Peter Cottle <pcottle@fb.com>
2014-06-30 20:51:09 -0700

    rebuild for prod

--- build/bundle.js
+++ build/bundle.js
@@ -10972,13 +10972,6 @@ GitEngine.prototype.rebaseInteractive = function(targetSource, currentLocation, 
   // there are a reduced set of checks now, so we can't exactly use parts of the rebase function
   // but it will look similar.
 
-  // first if we are upstream of the target
-  if (this.isUpstreamOf(targetSource, currentLocation)) {
-    throw new GitError({
-      msg: intl.str('git-result-nothing')
-    });
-  }
-
   // now get the stop set
   var stopSet = Graph.getUpstreamSet(this, targetSource);
 
@@ -36073,4 +36066,4 @@ exports.level = {
   }
 };
 
-},{}]},{},[11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96])
+},{}]},{},[11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,27,26,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,58,57,59,61,60,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96])
