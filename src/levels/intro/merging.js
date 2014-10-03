aaffe5b24ae652512c1c4f2da36318008ec5c4f5
i <isomorphisms@sdf.org>
2014-10-02 21:54:19 -0400

    suggestions for wording
    what does "two unique parents" mean?
    
    what does "include" mean?
    
    merge master into bugfix or bugfix into master?

--- src/levels/intro/merging.js
+++ src/levels/intro/merging.js
@@ -33,11 +33,11 @@ exports.level = {
             "markdowns": [
               "## Branches and Merging",
               "",
-              "Great! We now know how to commit and branch. Now we need to learn some kind of way of combining the work from two different branches together. This will allow us to branch off, develop a new feature, and then combine it back in.",
+              "Great! You now know how to commit and branch. Next: how to combine the work from two different branches together. Knowing this will allow you to branch off, develop a new feature, and then join it back to the main line of development.",
               "",
-              "The first method to combine work that we will examine is `git merge`. Merging in Git creates a special commit that has two unique parents. A commit with two parents essentially means \"I want to include all the work from this parent over here and this one over here, *and* the set of all their parents.\"",
+              "First we'll show you `git merge`. Merging in Git creates a special commit that has two unique parents. A commit with two parents essentially means \"I want to inherit from all the work from this parent and that parent, *and* all their parents.\"",
               "",
-              "It's easier with visuals, let's check it out in the next view"
+              "It's easier with visuals, let's check it out in the next view."
             ]
           }
         },
@@ -45,16 +45,16 @@ exports.level = {
           "type": "GitDemonstrationView",
           "options": {
             "beforeMarkdowns": [
-              "Here we have two branches; each has one commit that's unique. This means that neither branch includes the entire set of \"work\" in the repository that we have done. Let's fix that with merge.",
+              "Here we have two branches; each points to one commit. This means that neither branch points to the \"work history\". Let's fix that with merge.",
               "",
               "We will `merge` the branch `bugFix` into `master`"
             ],
             "afterMarkdowns": [
               "Woah! See that? First of all, `master` now points to a commit that has two parents. If you follow the arrows up the commit tree from `master`, you will hit every commit along the way to the root. This means that `master` contains all the work in the repository now.",
               "",
-              "Also, see how the colors of the commits changed? To help with learning, I have included some color coordination. Each branch has a unique color. Each commit turns a color that is the blended combination of all the branches that contain that commit.",
+              "Also, see how the colors of the commits changed? To help you see what happened, I color-coded the commits. Each branch gets one color. The commits' colors are a blend of all branches that contain that commit.",
               "",
-              "So here we see that the `master` branch color is blended into all the commits, but the `bugFix` color is not. Let's fix that..."
+              "So here we see that the `master` branch color (purple) is blended into all the commits, but the `bugFix` color (pink) is not. Let's fix that..."
             ],
             "command": "git merge bugFix",
             "beforeCommand": "git checkout -b bugFix; git commit; git checkout master; git commit"
@@ -64,12 +64,12 @@ exports.level = {
           "type": "GitDemonstrationView",
           "options": {
             "beforeMarkdowns": [
-              "Let's merge `master` into `bugFix`:"
+              "Let's merge `bugFix` into `master`:"
             ],
             "afterMarkdowns": [
-              "Since `bugFix` was an ancestor of `master`, git didn't have to do any work; it simply just moved `bugFix` to the same commit `master` was attached to.",
+              "Since `bugFix` was an ancestor of `master`, git didn't have to do anything to the snapshots; it just moved the `bugFix` pointer to the same commit `master` was attached to.",
               "",
-              "Now all the commits are the same color, which means each branch contains all the work in the repository! Woohoo!"
+              "Now all the commits are the same color, which means both branches contain the entire repository history. Woohoo!"
             ],
             "command": "git checkout bugFix; git merge master",
             "beforeCommand": "git checkout -b bugFix; git commit; git checkout master; git commit; git merge bugFix"
