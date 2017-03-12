df39090622dae2e02f3868f288c0e3ea9161019a
Peter Cottle <pcottle@fb.com>
2017-03-12 09:50:55 -0700

    rebuild for prod

--- build/bundle.js
+++ build/bundle.js
@@ -28905,7 +28905,7 @@ var intlRefresh = function() {
   $('span.intl-aware').each(function(i, el) {
     var intl = require('../intl');
     var key = $(el).attr('data-intl');
-    $(el).text(intl.str(key).toUpperCase());
+    $(el).text(intl.str(key));
   });
 };
 
