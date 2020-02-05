462656433bbf5e222bb3c12d5254b883867e5408
zengdabo <zengdb@gmail.com>
2020-02-05 16:11:43 +0800

    add simple chinese translation to lockedMaster

--- src/levels/remote/lockedMaster.js
+++ src/levels/remote/lockedMaster.js
@@ -3,10 +3,12 @@ exports.level = {
   "solutionCommand": "git reset --hard o/master;git checkout -b feature C2; git push origin feature",
   "startTree": "{\"branches\":{\"master\":{\"target\":\"C2\",\"id\":\"master\",\"remoteTrackingBranchID\":\"o/master\"},\"o/master\":{\"target\":\"C1\",\"id\":\"o/master\",\"remoteTrackingBranchID\":null}},\"commits\":{\"C0\":{\"parents\":[],\"id\":\"C0\",\"rootCommit\":true},\"C1\":{\"parents\":[\"C0\"],\"id\":\"C1\"},\"C2\":{\"parents\":[\"C1\"],\"id\":\"C2\"}},\"tags\":{},\"HEAD\":{\"target\":\"master\",\"id\":\"HEAD\"},\"originTree\":{\"branches\":{\"master\":{\"target\":\"C1\",\"id\":\"master\",\"remoteTrackingBranchID\":null}},\"commits\":{\"C0\":{\"parents\":[],\"id\":\"C0\",\"rootCommit\":true},\"C1\":{\"parents\":[\"C0\"],\"id\":\"C1\"}},\"tags\":{},\"HEAD\":{\"target\":\"master\",\"id\":\"HEAD\"}}}",
   "hint": {
-    "en_US": "Make the feature branch from the local master before resetting it back to be the same as origin's master"
+    "en_US": "Make the feature branch from the local master before resetting it back to be the same as origin's master",
+    "zh_CN": "从本地的master创建一个feature分支, 然后重置master和origin master保持一致。"
   },
   "name": {
-    "en_US": "Locked Master"
+    "en_US": "Locked Master",
+    "zh_CN": "锁定的Master(Locked Master)"
   },
   "startDialog": {
     "en_US": {
@@ -44,6 +46,47 @@ exports.level = {
               "## The solution",
               "",
               "Create another branch called feature and push that to the remote. Also reset your master back to be in sync with the remote otherwise you may have issues next time you do a pull and someone else's commit conflicts with yours."
+            ]
+          }
+        }
+      ]
+    }
+    
+    "zh_CN": {
+      "childViews": [
+        {
+          "type": "ModalAlert",
+          "options": {
+            "markdowns": [
+              "## 远程服务器拒绝!(Remote Rejected)",
+              "",
+              "如果你是在一个大的合作团队中工作, 很可能是master被锁定了, 需要一些Pull Request流程来合并修改。如果你直接提交(commit)到本地master, 然后试图推送(push)修改, 你将会收到这样类似的信息:",
+              "",
+              "```",
+              " ! [远程服务器拒绝] master -> master (TF402455: 不允许推送(push)这个分支; 你必须使用pull request来更新这个分支.)",
+              "```"
+            ]
+          }
+        },
+        {
+          "type": "ModalAlert",
+          "options": {
+            "markdowns": [
+              "## 为什么会被拒绝?",
+              "",
+              "远程服务器拒绝直接推送(push)提交到master, 因为策略配置要求 pull requests 来提交更新.",
+              "",
+              "你应该按照流程,新建一个分支, 推送(push)这个分支并申请pull request,但是你忘记并直接提交给了master.现在你卡住并且无法推送你的更新."
+            ]
+          }
+        },
+        {
+          "type": "ModalAlert",
+          "options": {
+            "markdowns": [
+              "## 解决办法",
+              "",
+              "新建一个分支feature, 推送到远程服务器. 然后reset你的master分支和远程服务器保持一致, 否则下次你pull并且他人的提交和你冲突的时候就会有问题."
             ]
           }
         }
