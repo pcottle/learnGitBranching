8eaa9f285c5259088371365278072817becd2c56
hongarc <aqua.hong.97@gmail.com>
2020-03-05 12:09:28 +0700

    feat: use node.js module instead of

--- src/js/intl/checkStrings.js
+++ src/js/intl/checkStrings.js
@@ -1,54 +1,28 @@
-var sys = require('sys');
+var { join } = require('path');
+var { readFileSync } = require('fs');
+
 var util = require('../util');
-var child_process = require('child_process');
-var strings = require('../intl/strings').strings;
+var { strings } = require('../intl/strings');
 
-var searchCommand = 'grep -C 2 -r "intl.str(" ../../';
-var genBadKeyCommand = function(key) {
-  return 'grep -r "' + key + '" ../../';
-};
-
-var easyRegex = /intl.str\('([a-zA-Z\-]+)'/g;
-var hardRegex = /\s+'([a-z\-]+)',/g;
-
-var findKey = function(badKey) {
-  child_process.exec(genBadKeyCommand(badKey), function(err, output) {
-    console.log(output);
-  });
-};
+var easyRegex = /intl\.str\(\s*'([a-zA-Z\-]+)'/g;
 
 var goodKeys = 0;
 var validateKey = function(key) {
   if (!strings[key]) {
     console.log('NO KEY for: "', key, '"');
-    findKey(key);
   } else {
     goodKeys++;
   }
 };
 
-var processLines = function(lines) {
-  lines.forEach(function(line) {
-    var results = easyRegex.exec(line);
-    if (results && results[1]) {
-      validateKey(results[1]);
-      return;
-    }
-    // could be a multi-liner
-    results = hardRegex.exec(line);
-    if (results && results[1]) {
-      validateKey(results[1]);
+if (!util.isBrowser()) {
+  util.readDirDeep(join(__dirname, '../..')).forEach(function(path) {
+    var content = readFileSync(path);
+    var match;
+    while (match = easyRegex.exec(content)) {
+      console.log(match[1])
+      validateKey(match[1]);
     }
   });
-};
-
-if (!util.isBrowser()) {
-  child_process.exec(
-    searchCommand,
-    function(err, output) {
-      processLines(output.split('\n'));
-      console.log(goodKeys + ' good keys found!');
-    }
-  );
+  console.log(goodKeys + ' good keys found!');
 }
-
