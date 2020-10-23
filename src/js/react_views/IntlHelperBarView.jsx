cdc0868282d12e13e1cd74f07e7692d26b97f91e
Olsza <olsza@users.noreply.github.com>
2020-10-23 08:34:29 +0200

    exit menu was set at the end

--- src/js/react_views/IntlHelperBarView.jsx
+++ src/js/react_views/IntlHelperBarView.jsx
@@ -115,15 +115,15 @@ class IntlHelperBarView extends React.Component{
         this.fireCommand('locale gl; levels');
       }.bind(this)
     }, {
-      icon: 'signout',
-      onClick: function() {
-        this.props.onExit();
-      }.bind(this)
-    }, {
       text: 'Slovensko',
       testID: 'slovenian',
       onClick: function() {
         this.fireCommand('locale sl_SI; levels');
+      }.bind(this)
+    }, {
+      icon: 'signout',
+      onClick: function() {
+        this.props.onExit();
       }.bind(this)
     }
     ];
