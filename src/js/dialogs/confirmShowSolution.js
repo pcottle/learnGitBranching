0332d6f038b55741136278135c0610d59877635e
unknown <shardanasoft@gmail.com>
2021-03-11 10:20:47 +0100

    firsts translations

--- src/js/dialogs/confirmShowSolution.js
+++ src/js/dialogs/confirmShowSolution.js
@@ -1,179 +1,217 @@
 exports.dialog = {
-  'en_US': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Are you sure you want to see the solution?',
-        '',
-        'I believe in you! You can do it'
-      ]
-    }
-  }],
-  'de_DE': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Bist du sicher, dass du die Auflösung sehen willst?',
-        '',
-        'Ich glaube an dich! Du schaffst das!'
-      ]
-    }
-  }],
-  'zh_CN': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## 确定要看答案吗？',
-        '',
-        '相信自己，你可以的！'
-      ]
-    }
-  }],
-  'zh_TW': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## 確定偷看解答嗎？',
-        '',
-        '我相信你！你可以的'
-      ]
-    }
-  }],
-  'es_AR': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## ¿Estás seguro de que querés ver la solución?',
-        '',
-        '¡Creo en vos! ¡Dale que podés!'
-      ]
-    }
-  }],
-  'es_MX': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## ¿Estás seguro de que quieres ver la solución?',
-        '',
-        '¡Creo en ti! ¡Yo sé que puedes!'
-      ]
-    }
-  }],
-  'es_ES': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## ¿Estás seguro de que quieres ver la solución?',
-        '',
-        '¡Creo en ti! ¡Ánimo!'
-      ]
-    }
-  }],
-  'pt_BR': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Tem certeza que quer ver a solução?',
-        '',
-        'Vamos lá, acredito que você consegue!'
-      ]
-    }
-  }],
-  'gl': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## ¿Queres ver a solución?',
-        '',
-        'Seguro que podes, ¡inténtao unha vez máis!'
-      ]
-    }
-  }],
-  'fr_FR': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Êtes-vous sûr de vouloir voir la solution ?',
-        '',
-        'Je crois en vous ! Vous pouvez le faire !'
-      ]
-    }
-  }],
-  'ja': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## どうしても正解がみたいですか?',
-        '',
-        '頑張れ頑張れできるできる絶対できる頑張れもっとやれるって',
-        '',
-        'やれる気持ちの問題だ頑張れ頑張れそこだ！',
-        '',
-        'そこで諦めるな絶対に頑張れ積極的にポジティブに頑張る頑張る',
-        '',
-        '北京だって頑張ってるんだから！'
-      ]
-    }
-  }],
-  'ru_RU': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Уверен, что хочешь посмотреть решение?',
-        '',
-        'Мы верим в тебя! Не прыгай! Ты сможешь!'
-      ]
-    }
-  }],
-  'uk': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Впевнений, що хочеш побачити розв’язок?',
-        '',
-        'Я вірю в тебе! Ти впораєшся!'
-      ]
-    }
-  }],
-  'vi': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Bạn chắc là muốn xem đáp án chứ?',
-        '',
-        'Tôi tin ở bạn! Bạn có thể làm được!'
-      ]
-    }
-  }],
-  'sl_SI': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Si prepričan, da hočeš videti rešitev?',
-        '',
-        'Verjamem vate! Maš ti to! Ali pač ne?'
-      ]
-    }
-  }],
-  'pl': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## Czy na pewno chcesz zobaczyć rozwiązanie?',
-        '',
-        'Wierzę w Ciebie! Możesz to zrobić'
-      ]
-    }
-  }],
-    'ta_IN': [{
-    type: 'ModalAlert',
-    options: {
-      markdowns: [
-        '## நீங்கள் நிச்சயமாக தீர்வை காண விரும்புகிறீர்களா?',
-        '',
-        'நான் உங்களால் அதை செய்ய முடியும் என நினைக்கிறேன்!'
-      ]
-    }
-  }],
+  en_US: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Are you sure you want to see the solution?",
+          "",
+          "I believe in you! You can do it",
+        ],
+      },
+    },
+  ],
+  de_DE: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Bist du sicher, dass du die Auflösung sehen willst?",
+          "",
+          "Ich glaube an dich! Du schaffst das!",
+        ],
+      },
+    },
+  ],
+  zh_CN: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: ["## 确定要看答案吗？", "", "相信自己，你可以的！"],
+      },
+    },
+  ],
+  zh_TW: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: ["## 確定偷看解答嗎？", "", "我相信你！你可以的"],
+      },
+    },
+  ],
+  es_AR: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## ¿Estás seguro de que querés ver la solución?",
+          "",
+          "¡Creo en vos! ¡Dale que podés!",
+        ],
+      },
+    },
+  ],
+  es_MX: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## ¿Estás seguro de que quieres ver la solución?",
+          "",
+          "¡Creo en ti! ¡Yo sé que puedes!",
+        ],
+      },
+    },
+  ],
+  es_ES: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## ¿Estás seguro de que quieres ver la solución?",
+          "",
+          "¡Creo en ti! ¡Ánimo!",
+        ],
+      },
+    },
+  ],
+  pt_BR: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Tem certeza que quer ver a solução?",
+          "",
+          "Vamos lá, acredito que você consegue!",
+        ],
+      },
+    },
+  ],
+  gl: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## ¿Queres ver a solución?",
+          "",
+          "Seguro que podes, ¡inténtao unha vez máis!",
+        ],
+      },
+    },
+  ],
+  fr_FR: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Êtes-vous sûr de vouloir voir la solution ?",
+          "",
+          "Je crois en vous ! Vous pouvez le faire !",
+        ],
+      },
+    },
+  ],
+  ja: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## どうしても正解がみたいですか?",
+          "",
+          "頑張れ頑張れできるできる絶対できる頑張れもっとやれるって",
+          "",
+          "やれる気持ちの問題だ頑張れ頑張れそこだ！",
+          "",
+          "そこで諦めるな絶対に頑張れ積極的にポジティブに頑張る頑張る",
+          "",
+          "北京だって頑張ってるんだから！",
+        ],
+      },
+    },
+  ],
+  ru_RU: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Уверен, что хочешь посмотреть решение?",
+          "",
+          "Мы верим в тебя! Не прыгай! Ты сможешь!",
+        ],
+      },
+    },
+  ],
+  uk: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Впевнений, що хочеш побачити розв’язок?",
+          "",
+          "Я вірю в тебе! Ти впораєшся!",
+        ],
+      },
+    },
+  ],
+  vi: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Bạn chắc là muốn xem đáp án chứ?",
+          "",
+          "Tôi tin ở bạn! Bạn có thể làm được!",
+        ],
+      },
+    },
+  ],
+  sl_SI: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Si prepričan, da hočeš videti rešitev?",
+          "",
+          "Verjamem vate! Maš ti to! Ali pač ne?",
+        ],
+      },
+    },
+  ],
+  pl: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Czy na pewno chcesz zobaczyć rozwiązanie?",
+          "",
+          "Wierzę w Ciebie! Możesz to zrobić",
+        ],
+      },
+    },
+  ],
+  ta_IN: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## நீங்கள் நிச்சயமாக தீர்வை காண விரும்புகிறீர்களா?",
+          "",
+          "நான் உங்களால் அதை செய்ய முடியும் என நினைக்கிறேன்!",
+        ],
+      },
+    },
+  ],
+  it_IT: [
+    {
+      type: "ModalAlert",
+      options: {
+        markdowns: [
+          "## Sicuro di voler sbirciare la soluzione?",
+          "",
+          "Io credo in te, dai che ce la fai!",
+        ],
+      },
+    },
+  ],
 };
 
