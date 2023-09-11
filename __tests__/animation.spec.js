ff0e2195bc3097a89c485b1129f14959a34827a3
Piotr.Bialas <bialas.Piotrek@outlook.com>
2023-09-11 11:39:19 +0200

    7

--- __tests__/animation.spec.js
+++ __tests__/animation.spec.js
@@ -19,7 +19,7 @@ describe('Promise animation', function() {
   });
 
   it('also takes animation packs', function() {
-    var value = 7;
+    var value = 8;
     var animation = new PromiseAnimation({
       animation: function() { value++; }
     });
