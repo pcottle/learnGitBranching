1dd74da56274ff2d733fbdb2077301f41cf74449
Peter Cottle <pcottle@meta.com>
2026-01-13 15:50:17 -0500

    more tests

--- __tests__/visuals.spec.js
+++ __tests__/visuals.spec.js
@@ -1,5 +1,6 @@
 
 var VisBranch = require('../src/js/visuals/visBranch');
+var GitVisuals = require('../src/js/visuals').GitVisuals;
 
 describe('visBranch', function() {
   describe('getBranchColor', function() {
@@ -28,4 +29,107 @@ describe('visBranch', function() {
     });
   });
 
+});
+
+describe('GitVisuals', function() {
+  it('removeBranch removes the matching visual branch', function() {
+    var targetBranch = { id: 'foo' };
+    var otherBranch = { id: 'bar' };
+    var removed = false;
+
+    var visBranch = {
+      get: function(key) {
+        if (key === 'branch') return targetBranch;
+      },
+      remove: function() {
+        removed = true;
+      }
+    };
+
+    var otherVisBranch = {
+      get: function(key) {
+        if (key === 'branch') return otherBranch;
+      },
+      remove: function() {}
+    };
+
+    var fake = {
+      gitEngine: {},
+      gitReady: true,
+      visBranchCollection: {
+        models: [visBranch, otherVisBranch],
+        each: function(callback, context) {
+          this.models.forEach(callback, context);
+        },
+        remove: function(model) {
+          var index = this.models.indexOf(model);
+          if (index > -1) this.models.splice(index, 1);
+        }
+      },
+      removeVisBranch: function(toRemove) {
+        this.visBranchCollection.remove(toRemove);
+      }
+    };
+
+    GitVisuals.prototype.removeBranch.call(fake, targetBranch);
+
+    expect(removed).toBe(true);
+    expect(fake.visBranchCollection.models).not.toContain(visBranch);
+    expect(fake.visBranchCollection.models).toContain(otherVisBranch);
+  });
+
+  it('removeBranch no-ops when the branch is not found', function() {
+    var missingBranch = { id: 'missing' };
+    var removed = false;
+
+    var visBranch = {
+      get: function(key) {
+        if (key === 'branch') return { id: 'present' };
+      },
+      remove: function() {
+        removed = true;
+      }
+    };
+
+    var fake = {
+      gitEngine: {},
+      gitReady: true,
+      visBranchCollection: {
+        models: [visBranch],
+        each: function(callback, context) {
+          this.models.forEach(callback, context);
+        },
+        remove: function(model) {
+          var index = this.models.indexOf(model);
+          if (index > -1) this.models.splice(index, 1);
+        }
+      },
+      removeVisBranch: function(toRemove) {
+        this.visBranchCollection.remove(toRemove);
+      }
+    };
+
+    GitVisuals.prototype.removeBranch.call(fake, missingBranch);
+
+    expect(removed).toBe(false);
+    expect(fake.visBranchCollection.models.length).toBe(1);
+  });
+
+  it('removeBranch defers work when git is not ready', function() {
+    var deferred = [];
+    var fake = {
+      gitEngine: null,
+      gitReady: false,
+      visBranchCollection: { models: [], each: function() {} },
+      removeVisBranch: function() {},
+      defer: function(action) {
+        deferred.push(action);
+      }
+    };
+
+    GitVisuals.prototype.removeBranch.call(fake, { id: 'foo' });
+
+    expect(deferred.length).toBe(1);
+    expect(typeof deferred[0]).toBe('function');
+  });
 });
