pcottle/learnGitBranching

commit 32c80bdb87d5aaa41e70221718e960c941c04aee

Resolves #201 -- allow dashes and slashes in branch names

src/js/git/index.js
@@ -607,8 +607,15 @@ GitEngine.prototype.getDetachedHead = function() {
 };
 
 GitEngine.prototype.validateBranchName = function(name) {
+  // Lets escape some of the nasty characters
+  name = name.replace(/&#x2F;/g,"\/");
   name = name.replace(/\s/g, '');
-  if (!/^[a-zA-Z0-9]+$/.test(name)) {
+  // And then just make sure it starts with alpha-numeric,
+  // can contain a slash or dash, and then ends with alpha
+  if (
+    !/^(\w+[.\/\-]?)+\w+$/.test(name) ||
+    name.search('o/') === 0
+  ) {
     throw new GitError({
       msg: intl.str(
         'bad-branch-name',
@@ -1461,7 +1468,7 @@ GitEngine.prototype.pullFinishWithMerge = function(
   chain = chain.then(function() {
     return this.animationFactory.getDelayedPromise(300);
   }.bind(this));
-  
+
   chain = chain.then(function() {
     // highlight last commit on o/master to color of
     // local branch
@@ -2129,7 +2136,7 @@ GitEngine.prototype.getUpstreamDiffFromSet = function(stopSet, location) {
 GitEngine.prototype.getInteractiveRebaseCommits = function(targetSource, currentLocation) {
   var stopSet = Graph.getUpstreamSet(this, targetSource);
   var toRebaseRough = [];
-  
+
   // standard BFS
   var pQueue = [this.getCommitFromRef(currentLocation)];
 
@@ -2152,28 +2159,28 @@ GitEngine.prototype.getInteractiveRebaseCommits = function(targetSource, current
       toRebase.push(commit);
     }
   });
-  
+
   if (!toRebase.length) {
     throw new GitError({
       msg: intl.str('git-error-rebase-none')
     });
   }
-  
+
   return toRebase;
 };
 
 GitEngine.prototype.rebaseInteractiveTest = function(targetSource, currentLocation, options) {
   options = options || {};
-  
+
   // Get the list of commits that would be displayed to the user
   var toRebase = this.getInteractiveRebaseCommits(targetSource, currentLocation);
-  
+
   var rebaseMap = {};
   _.each(toRebase, function(commit) {
     var id = commit.get('id');
     rebaseMap[id] = commit;
   });
-  
+
   var rebaseOrder;
   if (options['interactiveTest'].length === 0) {
     // If no commits were explicitly specified for the rebase, act like the user didn't change anything
@@ -2182,7 +2189,7 @@ GitEngine.prototype.rebaseInteractiveTest = function(targetSource, currentLocati
   } else {
     // Get the list and order of commits specified
     var idsToRebase = options['interactiveTest'][0].split(',');
-    
+
     // Verify each chosen commit exists in the list of commits given to the user
     var extraCommits = [];
     rebaseOrder = [];
@@ -2193,20 +2200,20 @@ GitEngine.prototype.rebaseInteractiveTest = function(targetSource, currentLocati
         extraCommits.push(id);
       }
     });
-    
+
     if (extraCommits.length > 0) {
       throw new GitError({
         msg: intl.todo('Hey those commits dont exist in the set!')
       });
     }
   }
-  
+
   this.rebaseFinish(rebaseOrder, {}, targetSource, currentLocation);
 };
 
 GitEngine.prototype.rebaseInteractive = function(targetSource, currentLocation, options) {
   options = options || {};
-  
+
   // there are a reduced set of checks now, so we can't exactly use parts of the rebase function
   // but it will look similar.
   var toRebase = this.getInteractiveRebaseCommits(targetSource, currentLocation);
@@ -2234,7 +2241,7 @@ GitEngine.prototype.rebaseInteractive = function(targetSource, currentLocation, 
     this.animationQueue.start();
   }.bind(this))
   .done();
-  
+
   // If we have a solution provided, set up the GUI to display it by default
   var initialCommitOrdering;
   if (options.initialCommitOrdering && options.initialCommitOrdering.length > 0) {
@@ -2242,7 +2249,7 @@ GitEngine.prototype.rebaseInteractive = function(targetSource, currentLocation, 
     _.each(toRebase, function(commit) {
       rebaseMap[commit.get('id')] = true;
     });
-    
+
     // Verify each chosen commit exists in the list of commits given to the user
     initialCommitOrdering = [];
     _.each(options.initialCommitOrdering[0].split(','), function(id) {
@@ -2462,7 +2469,7 @@ GitEngine.prototype.checkout = function(idOrTarget) {
   if (type === 'tag') {
     target = target.get('target');
   }
-  
+
   this.HEAD.set('target', target);
 };
 

__tests__/git.spec.js
@@ -57,21 +57,21 @@ describe('Git', function() {
       '%7B%22branches%22%3A%7B%22master%22%3A%7B%22target%22%3A%22C2%22%2C%22id%22%3A%22master%22%7D%2C%22side%22%3A%7B%22target%22%3A%22C3%27%22%2C%22id%22%3A%22side%22%7D%7D%2C%22commits%22%3A%7B%22C0%22%3A%7B%22parents%22%3A%5B%5D%2C%22id%22%3A%22C0%22%2C%22rootCommit%22%3Atrue%7D%2C%22C1%22%3A%7B%22parents%22%3A%5B%22C0%22%5D%2C%22id%22%3A%22C1%22%7D%2C%22C2%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C2%22%7D%2C%22C3%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C3%22%7D%2C%22C3%27%22%3A%7B%22parents%22%3A%5B%22C2%22%5D%2C%22id%22%3A%22C3%27%22%7D%7D%2C%22HEAD%22%3A%7B%22target%22%3A%22side%22%2C%22id%22%3A%22HEAD%22%7D%7D'
     );
   });
-  
+
   it('Interactive rebase', function() {
     expectTreeAsync(
       'gc; git checkout -b side C1; gc; git rebase -i master --interactive-test',
       '%7B%22branches%22%3A%7B%22master%22%3A%7B%22target%22%3A%22C2%22%2C%22id%22%3A%22master%22%7D%2C%22side%22%3A%7B%22target%22%3A%22C3%27%22%2C%22id%22%3A%22side%22%7D%7D%2C%22commits%22%3A%7B%22C0%22%3A%7B%22parents%22%3A%5B%5D%2C%22id%22%3A%22C0%22%2C%22rootCommit%22%3Atrue%7D%2C%22C1%22%3A%7B%22parents%22%3A%5B%22C0%22%5D%2C%22id%22%3A%22C1%22%7D%2C%22C2%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C2%22%7D%2C%22C3%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C3%22%7D%2C%22C3%27%22%3A%7B%22parents%22%3A%5B%22C2%22%5D%2C%22id%22%3A%22C3%27%22%7D%7D%2C%22HEAD%22%3A%7B%22target%22%3A%22side%22%2C%22id%22%3A%22HEAD%22%7D%7D'
     );
   });
-  
+
   it('Interactive rebases with commit re-ordering', function() {
 		expectTreeAsync(
 			'gc;gc;git rebase -i C0 --interactive-test C3,C1',
       '%7B%22branches%22%3A%7B%22master%22%3A%7B%22target%22%3A%22C1%27%22%2C%22id%22%3A%22master%22%7D%7D%2C%22commits%22%3A%7B%22C0%22%3A%7B%22parents%22%3A%5B%5D%2C%22id%22%3A%22C0%22%2C%22rootCommit%22%3Atrue%7D%2C%22C1%22%3A%7B%22parents%22%3A%5B%22C0%22%5D%2C%22id%22%3A%22C1%22%7D%2C%22C2%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C2%22%7D%2C%22C3%22%3A%7B%22parents%22%3A%5B%22C2%22%5D%2C%22id%22%3A%22C3%22%7D%2C%22C3%27%22%3A%7B%22parents%22%3A%5B%22C0%22%5D%2C%22id%22%3A%22C3%27%22%7D%2C%22C1%27%22%3A%7B%22parents%22%3A%5B%22C3%27%22%5D%2C%22id%22%3A%22C1%27%22%7D%7D%2C%22tags%22%3A%7B%7D%2C%22HEAD%22%3A%7B%22id%22%3A%22HEAD%22%2C%22target%22%3A%22master%22%7D%7D'
 		);
 	});
-  
+
   it('Switch branch and execute interactive rebase', function() {
 		expectTreeAsync(
 			'git branch test;git rebase -i C0 test --interactive-test',
@@ -148,7 +148,7 @@ describe('Git', function() {
       '%7B%22branches%22%3A%7B%22master%22%3A%7B%22target%22%3A%22C2%22%2C%22id%22%3A%22master%22%7D%2C%22bugFix%22%3A%7B%22target%22%3A%22C3%27%22%2C%22id%22%3A%22bugFix%22%7D%7D%2C%22commits%22%3A%7B%22C0%22%3A%7B%22parents%22%3A%5B%5D%2C%22id%22%3A%22C0%22%2C%22rootCommit%22%3Atrue%7D%2C%22C1%22%3A%7B%22parents%22%3A%5B%22C0%22%5D%2C%22id%22%3A%22C1%22%7D%2C%22C2%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C2%22%7D%2C%22C3%22%3A%7B%22parents%22%3A%5B%22C1%22%5D%2C%22id%22%3A%22C3%22%7D%2C%22C3%27%22%3A%7B%22parents%22%3A%5B%22C2%22%5D%2C%22id%22%3A%22C3%27%22%7D%7D%2C%22HEAD%22%3A%7B%22target%22%3A%22master%22%2C%22id%22%3A%22HEAD%22%7D%7D'
     );
   });
-  
+
   it('checks out after an interactive rebase', function() {
     expectTreeAsync(
       'git commit; git checkout -b bugFix C1; git commit; git rebase -i master --interactive-test;git checkout master',
@@ -169,7 +169,7 @@ describe('Git', function() {
       '{"branches":{"master":{"target":"C1","id":"master"}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"}},"HEAD":{"target":"master","id":"HEAD"}}'
     );
   });
-  
+
   it('solves rebase level with interactive rebase', function() {
     expectTreeAsync(
       'git checkout -b bugFix;git commit;git checkout master;git commit;git checkout bugFix;git rebase -i master --interactive-test',
@@ -246,6 +246,13 @@ describe('Git', function() {
 			'{"branches":{"master":{"target":"C3","id":"master","remoteTrackingBranchID":null}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"},"C2":{"parents":["C1"],"id":"C2"},"C3":{"parents":["C2"],"id":"C3"}},"tags":{},"HEAD":{"target":"master","id":"HEAD"}}'
 		);
 	});
+
+  it('can handle slashes and dashes in branch names but doesnt allow o/', function() {
+		expectTreeAsync(
+      'git branch foo/bar; git commit; git checkout foo/bar; gc; go master; git merge foo/bar; go foo/bar; git checkout -b bar-baz; git commit; git branch o/foo',
+      '{"branches":{"master":{"target":"C4","id":"master","remoteTrackingBranchID":null},"foo/bar":{"target":"C3","id":"foo/bar","remoteTrackingBranchID":null},"bar-baz":{"target":"C5","id":"bar-baz","remoteTrackingBranchID":null}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"},"C2":{"parents":["C1"],"id":"C2"},"C3":{"parents":["C1"],"id":"C3"},"C4":{"parents":["C2","C3"],"id":"C4"},"C5":{"parents":["C3"],"id":"C5"}},"tags":{},"HEAD":{"target":"bar-baz","id":"HEAD"}}'
+		);
+  });
 
 });
 

commit b4e0d7d403615ebd60dc0f966cc658608683f06b

Allow single-character branch names
git branch b / git checkout -b x were rejected with "not allowed!".
The name regex ^(\w+[.\/\-]?)+\w+$ imposes a two-char minimum via the
trailing \w+; ^\w([.\/\-]?\w+)*$ restores single-char names (a
regression since 32c80bdb / #201) while keeping dash/slash support.

src/js/git/index.js
@@ -648,7 +648,7 @@ GitEngine.prototype.validateBranchName = function(name) {
   // And then just make sure it starts with alpha-numeric,
   // can contain a slash or dash, and then ends with alpha
   if (
-    !/^(\w+[.\/\-]?)+\w+$/.test(name) ||
+    !/^\w([.\/\-]?\w+)*$/.test(name) ||
     name.search('o/') === 0
   ) {
     throw new GitError({

__tests__/git.spec.js
@@ -403,6 +403,13 @@ describe('Git', function() {
     );
   });
 
+  it('allows single-character branch names', function() {
+    return expectTreeAsync(
+      'git branch b; git checkout -b x',
+      '{"branches":{"main":{"target":"C1","id":"main"},"b":{"target":"C1","id":"b"},"x":{"target":"C1","id":"x"}},"commits":{"C0":{"parents":[],"id":"C0","rootCommit":true},"C1":{"parents":["C0"],"id":"C1"}},"HEAD":{"target":"x","id":"HEAD"}}'
+    );
+  });
+
   describe('RevList', function() {
 
     it('requires at least 1 argument', function() {