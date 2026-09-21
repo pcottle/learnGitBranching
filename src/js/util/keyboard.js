b80ac6171288aeabe546129762d049825dd31844
Peter Cottle <petermcottle@gmail.com>
2026-09-20 21:42:31 -0400

    Revert "Merge branch 'perf/lazy-levels-locales' into main"
    This reverts commit 84d2f7c2631e42a179e31eb28e50148e6168601c, reversing
    changes made to a9915df0c4d54aed69e405b86c491ff0f0db90db.

--- src/js/util/keyboard.js
+++ src/js/util/keyboard.js
@@ -33,9 +33,6 @@ KeyboardListener.prototype.listen = function() {
 };
 
 KeyboardListener.prototype.mute = function() {
-  if (!this.listening) {
-    return;
-  }
   this.listening = false;
   Main.getEventBaton().releaseBaton('docKeydown', this.keydown, this);
   Main.getEventBaton().releaseBaton('onCloseButtonClick', this.onCloseButtonClick, this);
